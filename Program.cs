fcfea051d105d3178ca371c17fe66390e0eb4f9e
Olga Tubylova <ol.tubylova@gmail.com>
2024-04-09 10:42:50 +0200

    Добавили программу, решающую поставленную задачу

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿string[] arr = new string[] { "Hello", "2", "world", ":-)" };
+List<string> new_arr = new List<string>();
+
+for (int i = 0; i < arr.Length; i++)
+{
+    string str = arr[i];
+    if (str.Length <= 3)
+    {
+        new_arr.Add(str);
+    }
+}
+
+Console.WriteLine(string.Join(", ", new_arr));
